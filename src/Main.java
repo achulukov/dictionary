97e92ae4e4c06289f673af9e8fe9e0d28dd00e2e
Петр Петров <3033106@gmail.com>
2022-07-06 20:35:30 +0400

    commit changes

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        String words = "Matata akuna tiki taka porque no itiendo aragones dexp totoro";
-        String[] arrWords = words.split(" ");
-        List<String> listWords =  Arrays.stream(arrWords).sorted(Comparator.naturalOrder()).collect(Collectors.toList()); // Здесь мы видим пример монада
-        StringBuilder dictionary = new StringBuilder();
-        Function<String, String> convert = x -> x + " "; // Пример чистой функции
-        for (String word : listWords ) {
+        String words = "matata akuna tiki taka porque no itiendo aragones dexp totoro";
+
+        words = Arrays.stream(words.split(" "))
+                .distinct()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.joining(" ")); // Здесь мы видим пример монада
+
+        Function<String, String[]> convert = x -> x.split(" "); // Пример чистой функции
+        String[] dictionary = convert.apply(words);
+        for (String word : dictionary) {
             System.out.println(word);
-            dictionary.append(convert.apply(word));
         }
-        System.out.println(dictionary);
     }
 }
